ce69e6ab29d0f93b2b8e99c1ab714c72f4cc6c1e
devenkcrypt <venkatesh7122002@gmail.com>
2025-10-18 19:18:39 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WiFi Gamepad',
+      title: 'Wifi Gamepad',
       theme: ThemeData.dark(useMaterial3: true),
       home: const ConnectionPage(),
     );
